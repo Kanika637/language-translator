8c6f4861ed4f9942618676a1f4fb30f55944cda6
kanika637 <kanikagola26@gmail.com>
2022-01-08 13:05:31 +0530

    Translate files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect} from 'react'
+import { Router, Outlet } from 'react-location';
+import { routes, location } from "./router"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router routes={routes} location={location}>
+        <div className="App" >
+            <Outlet />     
+              
+        </div>
+        </Router>
   );
 }
 
---
+++ src/Copy.js
@@ -0,0 +1,69 @@
+import { useEffect, useState, useRef } from "react"
+import './copy.css';
+// import "intl-tel-input/build/css/intlTelInput.css"
+// import intlTelInput from "intl-tel-input"
+
+// SMS sending statuses
+const STATUSES = {
+  IDLE: "",
+  FAILED: "Failed to copy",
+  PENDING: "Copying...",
+  SUCCEEDED: "Finished copying",
+}
+
+
+
+
+function Copy ({readText}) {
+  const [smsText, setSmsText] = useState(readText)
+//   const [iti, setIti] = useState(null)
+  const [smsSendingStatus, setSmsSendingStatus] = useState(STATUSES.IDLE)
+  const inputRef = useRef(null)
+
+ 
+
+  // Send SMS when the send button is clicked
+  const handleSubmit = e => {
+    e.preventDefault()
+    e.stopPropagation()
+    // sendSMS()
+  }
+
+ 
+ const output=document.querySelector(".text_input");
+
+const copyText=(e)=>{
+
+  output.select();
+  output.setSelectionRange(0,999999999999999999);
+  document.execCommand("copy");
+};
+
+
+return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>Edit the recognized text:</div>
+        <div>
+          <textarea
+            rows="15"
+            cols="45"
+            name="name"
+            defaultValue={readText}
+            className="text_input"
+            onChange={e => setSmsText(e.target.value)}
+          />
+        </div>
+        
+        <div>
+          <button className="copy_button" onClick={copyText} disabled={smsSendingStatus == "Sending SMS..."} type="submit">Copy</button>
+        </div>
+      </form>
+      <div className="status">
+        {smsSendingStatus}
+      </div>
+    </div>
+  )
+}
+
+export default Copy
